49d4a5a459b0ba33f580fdd601789cf7cc3c9cec
eneyi <ruth.ikwu@brunel.ac.uk>
2019-12-01 22:18:16 +0000

    Pyquery Engines

--- adultwork_bot/adultwork_bot/spiders/adultwork.py
+++ adultwork_bot/adultwork_bot/spiders/adultwork.py
@@ -7,7 +7,6 @@ from scrapy.http import FormRequest, Request
 from scrapy.linkextractors import LinkExtractor
 
 
-
 class AdultworkSpider(scrapy.Spider):
     name = 'adultwork'
     allowed_domains = ['adultwork.com']
@@ -46,32 +45,45 @@ class AdultworkSpider(scrapy.Spider):
     def parse_profile(self, response):
         pq, item =  PyQuery(response.text), response.meta['item']
         item = AdultworkEngine().extract_profile(pq, item)
-        ## if field report is present, parse reviews
-        yield Request(url=item['ratings']['ratingsLink'], callback=self.parse_ratings, meta = {'item': item})
+
+        if item['ratings']['hasRatings']:
+            yield Request(url=item['ratings']['ratingsLink'], callback=self.parse_ratings, meta = {'item': item})
+        
+        if item['reviews']['hasReviews']:
+            item['reviews']['reviews'] = []
+            for reviewLink in item['reviews']['reviewLinks']:
+                yield Request(url=reviewLink, callback=self.parse_review, meta = {'item': item})
+
+        if item['tours']['hasTours']:
+            yield Request(url=item['tours']['toursLink'], callback=self.parse_tour, meta={'item': item})
+        yield item  
+
 
     '''Parse all ratings for each sex worker on page'''
     def parse_ratings(self, response):
         item, stored_ratings = response.meta['item'], db.connect()['ratings'].findall('userid')
 
         if item['ratings']['hasRatings'] and item['ratings']['userid'] not in stored_ratings:
-            item['userid']['ratings']
             pq = PyQuery(response.text)
             item = AdultworkEngine().extract_ratings(pq, item)
+
+            ## check for ratings page of other users on this user's ratings page
             oRatingsUrls = [i.attr('href').split('href=')[-1].replace("'", "") for i in pq('a[href*="/dlgViewRatings.asp?UserID"]').items()]
             if len(oRatingsUrls) > 0:
                 for oRatingUrl in oRatingsUrls:
                     yield Request(url=oRatingUrl, callback=self.parse_ratings, meta = {'item': item})
-        else:
-            pass
-        yield Request(url=item.get('reviewsLink'), callback=self.parse_reviews, meta = {'item': item})
+        yield item
 
-    
+
     '''Parse all reviews for each sex worker on page'''
-    def parse_reviews(self, response):
-        pass
+    def parse_review(self, response):
+        item, pq = response.meta['item'], PyQuery(response.text)
+        item = AdultworkEngine().extract_review(pq, item)
+        yield item
+
 
     '''Parse all sex worker's tours'''
-    def parse_tours(self, response):
-        item, res = response.meta['item'], response.text
-        item['tours']
-        pass
+    def parse_tour(self, response):
+        item, pq = response.meta['item'], PyQuery(response.text)
+        item = AdultworkEngine().extract_tour(pq, item)
+        yield item
--- adultwork_bot/adultwork_bot/utilities/engine.py
+++ adultwork_bot/adultwork_bot/utilities/engine.py
@@ -1,4 +1,5 @@
-# This class handle the extraction of data from pyquery objects for each data item
+''' This handles the extraction of data from pyquery objects for each data item '''
+
 import string
 import phonenumbers
 import requests
@@ -96,17 +97,7 @@ class AdultWorkEngine(object):
         if len(sms_prices) > 1:
             item['pricing'].append({"serviceType":'webcam', "price":sms_prices[0].split()[0], "duration": sms_prices[-1].split()[0]})
         
-       ## Reviews Data
-       item['reviews'] = {}
-       reviews = pq('a[href*="vFR"]')
-       item['reviews']['hasReviews'] = len(reviews) != 0
-       if item['reviews']['hasReviews']:
-           item['reviews']['reviews'] = []
-           reviewID = i.attr('href').split('vFR(')[-1].replace(')', '')
-           reviewLink = 'https://www.adultwork.com/ViewFieldReport.asp?FRID={}'.format(reviewID)
-           item['reviews']['reviews'] = [{"date":i.text(), "reviewID":reviewID, "reviewLink":reviewLink}]
-
-        ## Ratings Data
+       ## Ratings Data
         item['ratings'] = {}
         ratings_text = [i.strip().split()[0] for i in pq('a[onclick*="viewRating"]').attr('title').split('-')]
         item['ratings']['hasRatings'] = ratings_text[0].split()[0] != '0'
@@ -118,13 +109,25 @@ class AdultWorkEngine(object):
             item['ratings']['negative'] = ratings_text[1].split()[4]
             item['ratings']['disputes'] = ratings_text[1].split()[6]
 
+       
+       ## Reviews Data
+       item['reviews'] = {}
+       reviews = pq('a[href*="vFR"]')
+       item['reviews']['hasReviews'] = len(reviews) != 0
+       if item['reviews']['hasReviews']:
+           review_base_link = 'https://www.adultwork.com/ViewFieldReport.asp?FRID={}'
+           reviewLinks = [i.attr('href').split("(")[-1].replace(")", "") for i in pq('a[href*="vFR"]').items()]
+           item['reviews']['reviewLinks'] = [review_base_link.format(i) for i in reviewLinks]
+
+        
+
         ## Tours Data
         item['tours'] = {}
         item['tours']['tourLink'] = 'https://www.adultwork.com/dlgUserTours.asp?UserID={}'.format(item['userid'])
         item['tours']['hasTours'] = 'Sorry, this member does not have any tours' not in requests.get(item['tours']['tourLink']).text
         return item 
     
-    '''Extracts ratings data fields from pyquery object-pq and stores in data object - item '''
+    '''Extracts ratings data fields from pyquery object-pq and stores in data object-item '''
     def extract_ratings(self, pq, item):
         '''pq: PyQuery Object with data to extract'''
         '''item: item object that holds data'''
@@ -142,5 +145,76 @@ class AdultWorkEngine(object):
                     data = {"for": item['userid'], "service": service.attr('id'), "type": data[0], "by": data[1], "date": data[2], "role": data[3],"serviceType":data[4], "description": data[5]}
                     item['ratings']['ratings'].append(data)
         return item
+
+
+    '''Extracts reviews data fields from pyquery object-pq and stores in data object-item'''
+    def extract_review(self, pq, item):
+        '''pq: PyQuery Object with data to extract'''
+        '''item: item object that holds data'''
+        data = {}
+        data['for'] = pq('td.Label:contains("Report On:")').next().text().split("\xa0")[0]
+        data['by'] = pq('td.Label:contains("Report By:")').next().text()
+        data['meetDate'] = pq('td.Label:contains("Meet Date:")').next().text()
+        data['meetLocation'] = pq('td.Label:contains("Meet Location:")').next().text()
+        data['type'] =pq('td.Label:contains("Type:")').next().text()
+        data['duration'] = pq('td.Label:contains("Time Spent:")').next().text() or pq('td.Label:contains("Duration:")').next().text()
+        data['price'] = pq('td.Label:contains("Fee:")').next().text()
+        data['recommended'] = pq('td.Label:contains("recommend:")').next().text()
+        data['visitAgain'] = pq('td.Label:contains("visit again:")').next().text()
+        data['valueForMoney'] = pq('td.Label:contains("for money:")').next().text()
+        data['overallRatings'] = pq('td.Label:contains("Overall Rating:")').next().text().split()[0]
+
+        data['feedback']['venue'] = {}
+        data['feedback']['venue']['description'] = pq('td.Label:contains("About the Venue")').parent().next().next().text()
+        data['feedback']['venue']['score'] = pq('td.Label:contains("About the Venue")').next().text().split('Score: ')[-1]
+
+        data['feedback']['meeting'] = {}
+        data['feedback']['meeting']['description'] = pq('td.Label:contains("About the Meeting")').parent().next().next().text()
+        data['feedback']['meeting']['score'] = pq('td.Label:contains("About the Meeting")').next().text().split("Score: ")[-1]
+
+        ss = "About " + data["for"].lower()
+        wf = pq('td.Label:contains("{}")'.format(ss)).parent()
+
+        data['feedback']['worker']['attributes'] = {}
+
+        data['feedback']['worker']['attributes']['physical'] = {}
+        data['feedback']['worker']['attributes']['physical']['description'] = wf.next().next().next().text()
+        data['feedback']['worker']['attributes']['physical']['score'] = wf.next().next().text().split("Score: ")[-1]
+
+        data['feedback']['worker']['attributes']['personality'] = {}
+        data['feedback']['worker']['attributes']['personality']['description'] = wf.next().next().next().next().next().next().text()
+        data['feedback']['worker']['attributes']['personality']['score'] = wf.next().next().next().next().next().text().split("Score: ")[-1]
+
+        data['feedback']['worker']['attributes']['service'] = {}
+        data['feedback']['worker']['attributes']['service']['description'] = wf.next().next().next().next().next().next().next().next().next().text()
+        data['feedback']['worker']['attributes']['service']['score'] = wf.next().next().next().next().next().next().next().next().text().split("Score: ")[-1]
+        item['reviews']['reviews'].append(data)
+        return item
+
+    '''Extracts tours data fields from pyquery object-pq and stores in data object-item'''
+    def extract_tours(self, pq, item):
+        item['tours']['tours'] = []
+        tourDetails = pq('td.Padded:contains("Details")').parent()
+        for tourDetail in tourDetails.items():
+            cells = tourDetail('td')
+            data = {}
+            data['where'] = PyQuery(w[0][2]).text()
+            data['starts'] = PyQuery(w[0][3]).text()
+            data['ends'] = PyQuery(w[0][4]).text()
+            data['stops'] = PyQuery(w[0][5]).text()
+            item['tours']['tours'].append(data)
+        return item
+
+
+
+
+
+
+
+        
+
+
+
+        
 
 
